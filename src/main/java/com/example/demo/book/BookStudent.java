11c12d67ca61c24de36dc5fedcfe44b101d15802
damouu <mouadsehbaoui@gmail.com>
2025-09-07 22:41:48 +0200

    Feature #SD-218 BookService Done Unassigned Fixed Done
    creates the BookStudent entity.

--- src/main/java/com/example/demo/book/BookStudent.java
+++ src/main/java/com/example/demo/book/BookStudent.java
@@ -18,12 +18,16 @@ public class BookStudent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Getter
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "book_id")
     private Book book;
 
+    @Getter
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "student_id_card_id")
+    @JoinColumn(name = "student_id_card")
     private StudentIdCard studentIdCard;
 
     private Date borrow_request_date;
@@ -32,14 +36,12 @@ public class BookStudent {
     private Date borrow_return_date;
     private boolean granted_borrow_extend;
 
-    public BookStudent(StudentIdCard studentIdCard, Date borrow_request_date, Date borrow_start_date, Date borrow_end_date, Date borrow_return_date, boolean granted_borrow_extend) {
-        this.studentIdCard = studentIdCard;
+    public BookStudent(Date borrow_request_date, Date borrow_start_date, Date borrow_end_date, Date borrow_return_date, boolean granted_borrow_extend) {
         this.borrow_request_date = borrow_request_date;
         this.borrow_start_date = borrow_start_date;
         this.borrow_end_date = borrow_end_date;
         this.borrow_return_date = borrow_return_date;
         this.granted_borrow_extend = granted_borrow_extend;
     }
-
 
 }
