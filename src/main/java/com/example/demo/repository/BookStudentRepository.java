794217da0bf676583d5c1fdad6159ec3587c3db6
damouu <mouadsehbaoui@gmail.com>
2025-11-09 22:33:17 +0100

    #SD-222 Done Unassigned TestsPassed Done
    refactor: BookStudentRepository.java
    
    moves the BookStudentRepository class to the repository folder.
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

--- src/main/java/com/example/demo/repository/BookStudentRepository.java
+++ src/main/java/com/example/demo/repository/BookStudentRepository.java
@@ -1,8 +1,11 @@
-package com.example.demo.book;
+package com.example.demo.repository;
 
+import com.example.demo.model.BookMemberCard;
+import com.example.demo.model.MemberCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -25,5 +28,8 @@ public interface BookStudentRepository extends JpaRepository<BookMemberCard, Int
 
     @Query(value = "SELECT EXISTS (select 1 from book_member_card inner join book on book.id = book_member_card.book_id where book.book_uuid = :book_uuid AND book_member_card.borrow_return_date is null ) AS is_borrowed", nativeQuery = true)
     boolean existsByBookIsBorrowed(UUID book_uuid);
+
+    @Query(value = "SELECT EXISTS (select 1 FROM book_member_card b WHERE b.member_card = :memberCard and b.borrow_return_date is null ) AS has_borrows", nativeQuery = true)
+    boolean getBookMemberCardByBook(@Param("memberCard") MemberCard memberCard);
 
 }
