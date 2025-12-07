f61b7308a4ab4bb83a61774e06fd368f2c0e2d0b
damouu <mouadsehbaoui@gmail.com>
2025-12-07 19:22:52 +0100

    refactor: PaginationUtil.java (#SD-267)

--- src/main/java/com/example/demo/util/PaginationUtil.java
+++ src/main/java/com/example/demo/util/PaginationUtil.java
@@ -2,7 +2,6 @@ package com.example.demo.util;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import java.util.Map;
 
@@ -13,13 +12,9 @@ public class PaginationUtil {
         PageRequest pageable;
 
         int page = Integer.parseInt(allParams.getOrDefault("page", "0"));
-        int size = Integer.parseInt(allParams.getOrDefault("size", "10"));
-        String sortField = allParams.getOrDefault("sort", "publicationDate");
-        String sortDirection = allParams.getOrDefault("direction", "desc");
+        int size = Integer.parseInt(allParams.getOrDefault("size", "5"));
 
-        Sort sort = sortDirection.equalsIgnoreCase("asc") ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
-
-        pageable = PageRequest.of(page, size, sort);
+        pageable = PageRequest.of(page, size);
         return pageable;
     }
 }
