d7fdaea91c8c01c0b2513a3a0c72dabd4c4e7275
damouu <mouadsehbaoui@gmail.com>
2025-12-14 22:34:27 +0100

    Create: ReturnEventData (#SD-269)

---
+++ src/main/java/com/example/demo/dto/ReturnEventData.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+@Builder
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ReturnEventData {
+
+    @JsonProperty("notification_data")
+    private ReturnNotificationDataEvent returnNotificationDataEvent;
+
+    @JsonProperty("inventory_data")
+    private InventoryDataEvent inventoryData;
+}
