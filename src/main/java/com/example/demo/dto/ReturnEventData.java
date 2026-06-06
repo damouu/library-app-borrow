73c605c9283088a42f6b283029b1663ad22b7a5e
damouu <mouadsehbaoui@gmail.com>
2026-06-06 19:52:20 +0900

    Refactor ReturnEventData (#SD-280)

--- src/main/java/com/example/demo/dto/ReturnEventData.java
+++ src/main/java/com/example/demo/dto/ReturnEventData.java
@@ -1,8 +1,11 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -12,9 +15,14 @@ import lombok.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReturnEventData {
 
-    @JsonProperty("notification_data")
-    private ReturnNotificationDataEvent returnNotificationDataEvent;
+    private UUID memberCardUUID;
+    private UUID borrow_uuid;
+    private String borrow_start_date;
+    private String borrow_end_date;
+    private String borrow_return_date;
+    private Boolean return_lately;
+    private Integer days_late;
+    private BigDecimal late_fee;
 
-    @JsonProperty("inventory_data")
-    private InventoryDataEvent inventoryData;
+    private List<BookToDecrement> returned_books;
 }
