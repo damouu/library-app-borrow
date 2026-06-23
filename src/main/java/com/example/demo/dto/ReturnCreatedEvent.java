40589e48e307e6381b9d20c0ebd4bc827f329632
damouu <mouadsehbaoui@gmail.com>
2026-06-23 12:39:00 +0900

    Refactor: ReturnCreatedEvent (#SD-288)

--- src/main/java/com/example/demo/dto/ReturnCreatedEvent.java
+++ src/main/java/com/example/demo/dto/ReturnCreatedEvent.java
@@ -1,17 +1,8 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+public record ReturnCreatedEvent(
+        Metadata metadata,
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ReturnCreatedEvent {
-
-    private Metadata metadata;
-
-    private ReturnCreatedEventData data;
+        ReturnCreatedEventData data
+) {
 }
