805b01fb38fb1743fc41459cdece0161d7761fec
damouu <mouadsehbaoui@gmail.com>
2026-06-23 12:37:34 +0900

    Refactor: BookToDecrement (#SD-288)

--- src/main/java/com/example/demo/dto/BookToDecrement.java
+++ src/main/java/com/example/demo/dto/BookToDecrement.java
@@ -1,22 +1,9 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class BookToDecrement {
+public record BookToDecrement(
+        UUID book_uuid,
 
-    @JsonProperty("book_uuid")
-    private UUID book_uuid;
-
-    @JsonProperty("chapter_uuid")
-    private UUID chapter_uuid;
+        UUID chapter_uuid) {
 }
