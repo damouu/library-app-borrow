6d7950895021956207ab023d1e0885fbdca6f1a3
damouu <mouadsehbaoui@gmail.com>
2026-06-23 12:37:45 +0900

    Refactor: BorrowCreatedEvent (#SD-288)

--- src/main/java/com/example/demo/dto/BorrowCreatedEvent.java
+++ src/main/java/com/example/demo/dto/BorrowCreatedEvent.java
@@ -1,17 +1,12 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class BorrowCreatedEvent {
+public record BorrowCreatedEvent(
+        Metadata metadata,
 
-    private Metadata metadata;
-
-    private BorrowCreatedEventData data;
+        BorrowCreatedEventData data
+) {
 }
