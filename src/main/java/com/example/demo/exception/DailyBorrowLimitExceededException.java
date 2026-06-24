7c66598b1bebde957d1de870a01ea192d79563c2
damouu <mouadsehbaoui@gmail.com>
2026-06-24 13:26:23 +0900

    Refactor: DailyBorrowLimitExceededException (#SD-290)

--- src/main/java/com/example/demo/exception/DailyBorrowLimitExceededException.java
+++ src/main/java/com/example/demo/exception/DailyBorrowLimitExceededException.java
@@ -1,7 +1,7 @@
 package com.example.demo.exception;
 
 public class DailyBorrowLimitExceededException extends RuntimeException {
-    public DailyBorrowLimitExceededException() {
-        super("一日の借入限度額に達しました。");
+    public DailyBorrowLimitExceededException(String errorMessage) {
+        super(errorMessage);
     }
 }
